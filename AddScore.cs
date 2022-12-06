8419336e58b2d233deb622a817667bc427fe5668
Adrian Gutierrez <adriangutierrez23@yahoo.com>
2022-12-05 21:20:34 -0800

    Uploaded Files

---
+++ AddScore.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs;
+
+namespace LeaderboardFunction
+{
+    public static class AddScore
+    {
+        [FunctionName("AddScore")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            [Blob("leaderboardcontainer/leaderboard.json", FileAccess.Read, Connection = "AzureWebJobsStorage")] string leaderboardBlobString,
+            [Blob("leaderboardcontainer/leaderboard.json", FileAccess.Write, Connection = "AzureWebJobsStorage")] TextWriter leaderboardBlobTextWriter,
+            ILogger log)
+        {
+            log.LogInformation("AddScore");
+
+            Leaderboard leaderboard = JsonConvert.DeserializeObject<Leaderboard>(leaderboardBlobString);
+
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            LeaderboardSingle leaderboardSingle = JsonConvert.DeserializeObject<LeaderboardSingle>(requestBody);
+
+            leaderboard.leaderboardSingleList.Add(leaderboardSingle);
+
+            string saveBlobData = JsonConvert.SerializeObject(leaderboard);
+
+            leaderboardBlobTextWriter.Write(saveBlobData);
+
+            return new OkObjectResult(saveBlobData);
+        }
+    }
+}
---
+++ Leaderboard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaderboardFunction
+{
+    public class Leaderboard
+    {
+        public List<LeaderboardSingle> leaderboardSingleList;
+    }
+
+    public class LeaderboardSingle
+    {
+        public string name;
+        public int score;
+    }
+}
